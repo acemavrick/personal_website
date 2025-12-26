a7c9734bbf2057daa09705361776108ea18603b1
acemavrick <116685696+acemavrick@users.noreply.github.com>
2025-12-26 16:08:21 -0600

    small corrections

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,7 @@ export const site = {
   name: "Shubh Randeria",
   pfp: "https://avatars.githubusercontent.com/acemavrick",
   url: "https://randeria.dev",
-  tagline: `Hello! I'm Shubh, a high school senior. 
-  I like building software to learn new things and solve problems I encounter. 
-  This site shows some of what I've worked on. Feel free to look around and
-  don't hesitate to contact me if you have any feedback, questions, or just want to talk!`,
+  tagline: `Hello! I'm Shubh, a high school senior. I like building software to learn new things and solve problems I encounter.  This site shows some of what I've worked on. Feel free to look around and don't hesitate to contact me if you have any feedback, questions, or just want to talk!`,
   socials: [
     { href: "https://github.com/acemavrick", handle: "acemavrick", icon: "github" },
     { href: "https://www.youtube.com/channel/UCv9H6UivmaxVSGd0ayGwV7w", handle: "@acemavrick5020", icon: "youtube" },
